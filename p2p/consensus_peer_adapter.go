97c39529d8aa5112e3b99eb462795b8ac08d85fe
SPERAXIO <contact@sperax.io>
2020-05-26 17:29:25 +0800

    consensus peer adapter

--- p2p/consensus_peer_adapter.go
+++ p2p/consensus_peer_adapter.go
@@ -15,13 +15,13 @@ type p2pAddress string
 func (p2pAddress) Network() string     { return "p2p" }
 func (addr p2pAddress) String() string { return string(addr) }
 
-type Peer struct {
+type ConsensusPeerAdapter struct {
 	h     *Host
 	topic *libp2p_pubsub.Topic
 }
 
-func NewPeer(h *Host) (*Peer, error) {
-	p := new(Peer)
+func NewConsensusPeerAdapter(h *Host) (*ConsensusPeerAdapter, error) {
+	p := new(ConsensusPeerAdapter)
 	p.h = h
 	topic, err := h.GetOrJoin(ConsensusTopic)
 	if err != nil {
@@ -32,19 +32,19 @@ func NewPeer(h *Host) (*Peer, error) {
 	return p, nil
 }
 
-func (p *Peer) Topic() *libp2p_pubsub.Topic { return p.topic }
+func (p *ConsensusPeerAdapter) Topic() *libp2p_pubsub.Topic { return p.topic }
 
 // We adapt broadcasting scheme for consensus algorithm, so we need only ONE consensus peer for
 // message routing.
 // below are the interfaces adapter for consensus, will treat this p2p host as a peer for
 // broadcasting entry.
-func (p *Peer) GetPublicKey() *ecdsa.PublicKey { return &p.h.priKey.PublicKey }
+func (p *ConsensusPeerAdapter) GetPublicKey() *ecdsa.PublicKey { return &p.h.priKey.PublicKey }
 
 // RemoteAddr returns remote addr
-func (p *Peer) RemoteAddr() net.Addr { return p2pAddress(p.h.host.ID()) }
+func (p *ConsensusPeerAdapter) RemoteAddr() net.Addr { return p2pAddress(p.h.host.ID()) }
 
 // Send a msg to this peer
-func (p *Peer) Send(msg []byte) error {
+func (p *ConsensusPeerAdapter) Send(msg []byte) error {
 	go func() {
 		ctx := context.Background()
 		p.topic.Publish(ctx, msg)
