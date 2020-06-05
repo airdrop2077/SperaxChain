55e9fdcf514005d3ef5ebc51a7e11ee36070b3e4
SPERAXIO <contact@sperax.io>
2020-06-05 11:40:35 +0800

    remove difficulty field in header

--- core/types/gen_header_json.go
+++ core/types/gen_header_json.go
@@ -8,7 +8,10 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
+
+var _ = (*headerMarshaling)(nil)
 
 // MarshalJSON marshals as JSON.
 func (h Header) MarshalJSON() ([]byte, error) {
@@ -19,14 +22,14 @@ func (h Header) MarshalJSON() ([]byte, error) {
 		TxHash      common.Hash    `json:"transactionsRoot" gencodec:"required"`
 		ReceiptHash common.Hash    `json:"receiptsRoot"     gencodec:"required"`
 		Bloom       Bloom          `json:"logsBloom"        gencodec:"required"`
-		Difficulty  *big.Int       `json:"difficulty"       gencodec:"required"`
-		Number      *big.Int       `json:"number"           gencodec:"required"`
-		GasLimit    uint64         `json:"gasLimit"         gencodec:"required"`
-		GasUsed     uint64         `json:"gasUsed"          gencodec:"required"`
-		Time        uint64         `json:"timestamp"        gencodec:"required"`
-		Extra       []byte         `json:"extraData"        gencodec:"required"`
+		Number      *hexutil.Big   `json:"number"           gencodec:"required"`
+		GasLimit    hexutil.Uint64 `json:"gasLimit"         gencodec:"required"`
+		GasUsed     hexutil.Uint64 `json:"gasUsed"          gencodec:"required"`
+		Time        hexutil.Uint64 `json:"timestamp"        gencodec:"required"`
+		Extra       hexutil.Bytes  `json:"extraData"        gencodec:"required"`
 		MixDigest   common.Hash    `json:"mixHash"`
-		Decision    []byte         `json:"decision"           gencodec:"required"`
+		Decision    hexutil.Bytes  `json:"decision"           gencodec:"required"`
+		Hash        common.Hash    `json:"hash"`
 	}
 	var enc Header
 	enc.ParentHash = h.ParentHash
@@ -35,14 +38,14 @@ func (h Header) MarshalJSON() ([]byte, error) {
 	enc.TxHash = h.TxHash
 	enc.ReceiptHash = h.ReceiptHash
 	enc.Bloom = h.Bloom
-	enc.Difficulty = h.Difficulty
-	enc.Number = h.Number
-	enc.GasLimit = h.GasLimit
-	enc.GasUsed = h.GasUsed
-	enc.Time = h.Time
+	enc.Number = (*hexutil.Big)(h.Number)
+	enc.GasLimit = hexutil.Uint64(h.GasLimit)
+	enc.GasUsed = hexutil.Uint64(h.GasUsed)
+	enc.Time = hexutil.Uint64(h.Time)
 	enc.Extra = h.Extra
 	enc.MixDigest = h.MixDigest
 	enc.Decision = h.Decision
+	enc.Hash = h.Hash()
 	return json.Marshal(&enc)
 }
 
@@ -55,14 +58,13 @@ func (h *Header) UnmarshalJSON(input []byte) error {
 		TxHash      *common.Hash    `json:"transactionsRoot" gencodec:"required"`
 		ReceiptHash *common.Hash    `json:"receiptsRoot"     gencodec:"required"`
 		Bloom       *Bloom          `json:"logsBloom"        gencodec:"required"`
-		Difficulty  *big.Int        `json:"difficulty"       gencodec:"required"`
-		Number      *big.Int        `json:"number"           gencodec:"required"`
-		GasLimit    *uint64         `json:"gasLimit"         gencodec:"required"`
-		GasUsed     *uint64         `json:"gasUsed"          gencodec:"required"`
-		Time        *uint64         `json:"timestamp"        gencodec:"required"`
-		Extra       []byte          `json:"extraData"        gencodec:"required"`
+		Number      *hexutil.Big    `json:"number"           gencodec:"required"`
+		GasLimit    *hexutil.Uint64 `json:"gasLimit"         gencodec:"required"`
+		GasUsed     *hexutil.Uint64 `json:"gasUsed"          gencodec:"required"`
+		Time        *hexutil.Uint64 `json:"timestamp"        gencodec:"required"`
+		Extra       *hexutil.Bytes  `json:"extraData"        gencodec:"required"`
 		MixDigest   *common.Hash    `json:"mixHash"`
-		Decision    []byte          `json:"decision"           gencodec:"required"`
+		Decision    *hexutil.Bytes  `json:"decision"           gencodec:"required"`
 	}
 	var dec Header
 	if err := json.Unmarshal(input, &dec); err != nil {
@@ -92,36 +94,32 @@ func (h *Header) UnmarshalJSON(input []byte) error {
 		return errors.New("missing required field 'logsBloom' for Header")
 	}
 	h.Bloom = *dec.Bloom
-	if dec.Difficulty == nil {
-		return errors.New("missing required field 'difficulty' for Header")
-	}
-	h.Difficulty = dec.Difficulty
 	if dec.Number == nil {
 		return errors.New("missing required field 'number' for Header")
 	}
-	h.Number = dec.Number
+	h.Number = (*big.Int)(dec.Number)
 	if dec.GasLimit == nil {
 		return errors.New("missing required field 'gasLimit' for Header")
 	}
-	h.GasLimit = *dec.GasLimit
+	h.GasLimit = uint64(*dec.GasLimit)
 	if dec.GasUsed == nil {
 		return errors.New("missing required field 'gasUsed' for Header")
 	}
-	h.GasUsed = *dec.GasUsed
+	h.GasUsed = uint64(*dec.GasUsed)
 	if dec.Time == nil {
 		return errors.New("missing required field 'timestamp' for Header")
 	}
-	h.Time = *dec.Time
+	h.Time = uint64(*dec.Time)
 	if dec.Extra == nil {
 		return errors.New("missing required field 'extraData' for Header")
 	}
-	h.Extra = dec.Extra
+	h.Extra = *dec.Extra
 	if dec.MixDigest != nil {
 		h.MixDigest = *dec.MixDigest
 	}
 	if dec.Decision == nil {
 		return errors.New("missing required field 'decision' for Header")
 	}
-	h.Decision = dec.Decision
+	h.Decision = *dec.Decision
 	return nil
 }
