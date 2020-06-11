0522753f7a3518ca25adfef792dad8dc4315100f
SPERAXIO <contact@sperax.io>
2020-06-11 12:27:09 +0800

    make sure the work will be resubmitted if consensus cannot complete for some reason

--- miner/worker.go
+++ miner/worker.go
@@ -374,7 +374,7 @@ func (w *worker) newWorkLoop(recommit time.Duration) {
 		case <-timer.C:
 			// If mining is running resubmit a new work cycle periodically to pull in
 			// higher priced transactions. Disable this overhead for pending blocks.
-			if w.isRunning() && w.chainConfig.BDLS == nil {
+			if w.isRunning() {
 				// Short circuit if no new transaction arrives.
 				if atomic.LoadInt32(&w.newTxs) == 0 {
 					timer.Reset(recommit)
