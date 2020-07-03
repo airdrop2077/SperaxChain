78b5f2cdee1cc87703319b1b775b684be91c5807
SPERAXIO <contact@sperax.io>
2020-07-03 21:42:23 +0800

    propose block protocol extension

--- eth/protocol.go
+++ eth/protocol.go
@@ -44,7 +44,7 @@ const protocolName = "spa"
 var ProtocolVersions = []uint{spa01}
 
 // protocolLengths are the number of implemented message corresponding to different protocol versions.
-var protocolLengths = map[uint]uint64{spa01: 18, eth65: 17, eth64: 17, eth63: 17}
+var protocolLengths = map[uint]uint64{spa01: 19, eth65: 17, eth64: 17, eth63: 17}
 
 const protocolMaxMsgSize = 10 * 1024 * 1024 // Maximum cap on the size of a protocol message
 
@@ -73,6 +73,7 @@ const (
 
 	// BDLS Consensus Extensions
 	BDLSConsensusMsg = 0x11
+	ProposeBlock     = 0x12
 )
 
 type errCode int
